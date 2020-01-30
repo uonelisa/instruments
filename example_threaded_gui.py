f01ffa85cf928025ce5da00a8abe4b834c0d63f7
stupoole <stuart.f.poole@gmail.com>
2020-01-30 12:33:16 +0000

    Added stop button functionality properly?

--- example_threaded_gui.py
+++ example_threaded_gui.py
@@ -10,17 +10,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 
 
-# class Counter:
-#     def __init__(self):
-#         self.data = np.arange(0, 10000)
-#         self.data_ready = True
-#
-#     def get_data(self):
-#         return self.data
-#
-#     def loop(self):
-
-
+# This class is the worker which will do the data collection and return the data ready for plotting
+# This will contain all of the serial commanding and data control.
 class Worker(QtCore.QObject):
     finished = QtCore.pyqtSignal()
     dataReady = QtCore.pyqtSignal(np.ndarray)
@@ -28,46 +19,50 @@ class Worker(QtCore.QObject):
     data = np.arange(0, 10000)
     is_stopped = False
 
+
+    # This method is called by a button "start"
     @QtCore.pyqtSlot()
     def processB(self):
+        self.is_stopped = False
         print("Worker.processB()")
         i = 1
-        while (i < 10 and not self.is_stopped):
+        while i < 10 and not self.is_stopped:
             self.data = self.data * 1.1
             time.sleep(5)
             self.dataReady.emit(self.data)
             i += 1
-        self.finished.emit()
+        # self.finished.emit() ## this will call thread.quit if emitted
 
-
+# This is the function called when the dataRead.emit() is called in processB. This should be a plot window
 def onDataReady(array):
     plt.plot(array)
     plt.pause(0.01)
 
-
-app = QtWidgets.QApplication(sys.argv)
-
-thread = QtCore.QThread()
-obj = Worker()
-obj.dataReady.connect(onDataReady)
-
-obj.moveToThread(thread)
-
-obj.finished.connect(thread.quit)
-
-thread.start()
-
-
+# This starts processB method
 def on_start():
     QtCore.QMetaObject.invokeMethod(obj, 'processB', Qt.QueuedConnection)
 
-
+# This stops the processB method from doing another loop
 def on_stop():
-    thread.quit()
-    # TODO: replace this with only killing thread. thread.exit no worky
-    # TODO: Need to make a connection to allow process to end by updating worker's is_stopped
+    obj.is_stopped = True
 
+# Creates an application. Formality of QT
+app = QtWidgets.QApplication(sys.argv)
+# make a thread for the serial stuff to occur in
+thread = QtCore.QThread()
+# create a Qthread worker class which contains all possible methods
+obj = Worker()
+# Connect the data ready signal to the data onDataReady plotting function
+obj.dataReady.connect(onDataReady)
+# Move the worker class onto it's own thread
+obj.moveToThread(thread)
+# Connect the finished signal to a quit function to end the thread. (not used in this example)
+obj.finished.connect(thread.quit)
 
+# starts the worker thread.
+thread.start()
+
+# Draws a window, displays it, adds 2 buttons with callbacks and then resizes the window.
 window = QtWidgets.QWidget()
 window.setWindowTitle("Control Window")
 window.show()
@@ -80,7 +75,6 @@ layout.addWidget(start_button, 0, 0)
 layout.addWidget(stop_button, 1, 0)
 window.resize(480, 160)
 
-
-
+# Starts the application running it's callback loops etc.
 app.exec_()
 thread.finished.connect(app.exit)
