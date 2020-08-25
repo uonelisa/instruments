a619373ffec1c5cc67ee78d89dd9f22f3057d449
stupoole <stuart.f.poole@gmail.com>
2020-08-25 12:55:09 +0100

    added example

---
+++ example_TEC.py
@@ -0,0 +1,19 @@
+import instruments
+import time
+
+tec = instruments.TEC1089SV()
+tec.connect(14)
+
+print(f'object temp = {tec.get_object_temperature()}°C')
+print(f'sink temp = {tec.get_sink_temperature()}°C')
+print(f'target temp = {tec.get_target_temperature()}°C')
+print(f'response = "{tec.set_target_temperature(21)}"')
+
+print("stability: " + tec.get_temp_stability_state())
+tec.enable_control()
+time.sleep(20)
+print("stability: " + tec.get_temp_stability_state())
+tec.disable_control()
+
+print(f'object temp = {tec.get_object_temperature()}°C')
+print(f'sink temp = {tec.get_sink_temperature()}°C')
