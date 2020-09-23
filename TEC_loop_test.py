460d1f9f81de367b0b469dce536f44c6a81b20b6
stupoole <stuart.f.poole@gmail.com>
2020-09-23 14:28:51 +0100

    The TEC message counter was breaking the serial comms. I decided to remove that from get param but there must be an issue in the error checking or something.

---
+++ TEC_loop_test.py
@@ -0,0 +1,24 @@
+import instruments
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+
+tec = instruments.TEC1089SV()
+
+print(tec.connect(14))
+tec.set_target_temperature(21)
+a =
+n = 10
+temp = np.zeros(n)
+t = np.zeros(n)
+start_time = time.time()
+for i in range(n):
+    x = tec.get_object_temperature()
+    print(x)
+    temp[i] = x
+    t[i] = time.time() - start_time
+    # time.sleep(0.1)
+
+plt.plot(t, temp)
+plt.show()
