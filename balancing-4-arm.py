542f57499271430d0a8860d4709c1addd63c3ecb
stupoole <stuart.f.poole@gmail.com>
2020-10-01 14:49:53 +0100

    added 4 arm balancing script

--- balancing-4-arm.py
+++ balancing-4-arm.py
@@ -7,10 +7,10 @@ import instruments
 # output into your own script.
 
 
-# define all 2 opposite pairing assignments for 2 wire measurements
+# define all 4 opposite pairing assignments for 2 wire measurements
 
-assignments_top = {"I+": "F", "I-": "A"}
-assignments_bottom = {"I+": "E", "I-": "B"}
+assignments_top = {"I+": "A", "I-": "E"}
+assignments_bottom = {"I+": "C", "I-": "G"}
 
 
 # a dictionary to store the values in
@@ -23,12 +23,12 @@ balance_box = instruments.BalanceBox()
 pg = instruments.K2461()
 
 # connect to devices
-switch_box.connect(4)
-balance_box.connect(5)
+switch_box.connect(15)
+balance_box.connect(17)
 pg.connect()
 
-# set k2461 ready for 100uA probe current 2 wire with nplc 2
-pg.enable_2_wire_probe(1e-3, 2)
+# set k2461 ready for 1mA probe current 2 wire with nplc 2
+pg.enable_2_wire_probe(500e-6, 2)
 balance_box.enable_all()
 balance_box.reset_resistances()
 balance_box.set_resistances(resistance_assigned)
@@ -37,18 +37,18 @@ time.sleep(2)
 c, v = pg.read_one()
 print(int(v / c))
 resistance_measured["A"] = int((v / c) / 2)
-resistance_measured["F"] = int((v / c) / 2)
+resistance_measured["E"] = int((v / c) / 2)
 
 # set first assignment
 switch_box.switch(assignments_bottom)
 time.sleep(2)  # delay to allow the sb to switch
 c, v = pg.read_one()  # read one value
-resistance_measured["B"] = int((v / c) / 2)  # store half resistance rounded down in each channel part of the pair
-resistance_measured["E"] = int((v / c) / 2)
+resistance_measured["C"] = int((v / c) / 2)  # store half resistance rounded down in each channel part of the pair
+resistance_measured["G"] = int((v / c) / 2)
 
 
 print('resistance measured = ', resistance_measured)
-max_res = int(max(resistance_measured.values()) + 50)
+max_res = int(max(resistance_measured.values()) + 5)
 
 
 for pin, res in resistance_measured.items():
@@ -63,13 +63,13 @@ switch_box.switch(assignments_top)
 time.sleep(2)  # delay to allow the sb to switch
 c, v = pg.read_one()  # read one value
 resistance_measured["A"] = int((v / c) / 2)  # store half resistance rounded down in each channel part of the pair
-resistance_measured["F"] = int((v / c) / 2)
+resistance_measured["E"] = int((v / c) / 2)
 
 switch_box.switch(assignments_bottom)
 time.sleep(2)
 c, v = pg.read_one()
-resistance_measured["B"] = int((v / c) / 2)
-resistance_measured["E"] = int((v / c) / 2)
+resistance_measured["C"] = int((v / c) / 2)
+resistance_measured["G"] = int((v / c) / 2)
 
 
 print('New resistances = ', resistance_measured)
