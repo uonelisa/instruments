df707c45bb9e012f8a627ff4a653b3e4032737db
Spintronics User <ppxsp1@nottingham.ac.uk>
2021-05-21 11:00:09 +0100

    Some settings changes. Fixed syntax for range settings 6221

--- delta_pulse_customsweep.py
+++ delta_pulse_customsweep.py
@@ -10,13 +10,15 @@ from tkinter import filedialog as dialog
 
 frequency = 1
 
-I_max = 10e-3  # Used 17mA for 5um LXB025 UJ.
-step = 0.25e-3  # Used 0.5mA steps for 5um LXB025 UJ.
+I_max = 15e-3  # Used 15mA to RC124 10um, 10mA for RC123 10um UJ, 5mA for RC123 5um UJ
+step = 0.25e-3  # Used 0.1mA steps for 5um RC123 UJ otherwise 0.25mA
 delay = 1
 repeats = 20
 channel = 1
 volt_range = 100e-3  # 10 for Rxx # 100e-3 for Rxy sweep 1
 width = 500e-6
+compliance = 40
+bias = 0.0
 
 curr_list = np.concatenate((np.linspace(0, I_max, round(I_max / step) + 1),
                             np.linspace(I_max - step, -I_max, round(2 * I_max / step)),
@@ -26,7 +28,7 @@ source = instruments.K6221_Ethernet()
 source.connect()
 source.set_compliance(60)
 source.set_sense_chan_and_range(channel, volt_range)
-source.configure_custom_sweep(curr_list[1::], delay, 40, repeats)
+source.configure_custom_sweep(curr_list[1::], delay, compliance, repeats, bias, 'best')
 source.configure_pulse(width, 1, 1)
 
 source.arm_pulse_sweep()
