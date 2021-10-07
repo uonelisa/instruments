bea177fe10cd46430cd358ded9fd4def3f18317d
stupoole <stuart.f.poole@gmail.com>
2021-10-07 15:37:25 +0100

    Adding the save dialogue box and filehandler.py

--- instruments/filehandler.py
+++ instruments/filehandler.py
@@ -5,6 +5,14 @@ __all__ = ['save']
 
 
 def save(data, header=None):
+    """
+    A simple method to load a dialogue box to allow the user to specify the save file name and location.
+
+    :param np.ndarray data: a numpy columnstacked matrix
+    :param str header : typically a comma separated string to describe each column and maybe contain experimental
+    parameters
+    :return:
+    """
     name = dialog.asksaveasfilename(title='Save data')
     if name:  # if a name was entered, don't save otherwise
         if name[-4:] != '.txt':  # add .txt if not already there
