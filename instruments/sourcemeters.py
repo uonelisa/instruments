0a1f275d9a084d1043b062a4773e95b5f9022ef1
stupoole <stuart.f.poole@gmail.com>
2021-11-04 14:18:23 +0000

    Removing additional zero added to create the array in 6221.configure_custom_sweep such that the first value is the first specified value instead of 0.

--- instruments/sourcemeters.py
+++ instruments/sourcemeters.py
@@ -690,8 +690,8 @@ class K6221:
         """
         self.K6221.write(f'source:current {bias}')
         self.K6221.write('source:sweep:spacing list')
-        self.K6221.write('source:list:current 0')
-        for x in sweep_list:
+        self.K6221.write(f'source:list:current {sweep_list[0]}')
+        for x in sweep_list[1:]:
             self.K6221.write(f'source:list:current:append {x}')
         self.K6221.write(f'source:list:delay {delay}')
         self.K6221.write(f'source:list:compliance {compliance}')
