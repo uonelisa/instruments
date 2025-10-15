9a7a092a7ad78e6a61856e029875fb6ed5f383d6
AlexKeene-03 <alexkeene03@gmail.com>
2025-10-15 14:47:52 +0100

    Add files via upload

---
+++ New_instruments/2 Wire current measurment.py
@@ -0,0 +1,40 @@
+import instruments
+import time
+import numpy as np
+from tkinter import filedialog as dialog
+import matplotlib.pyplot as plt
+
+sb = instruments.SwitchBox()
+sm = instruments.K2461()
+
+sb.connect(3)
+sm.connect()
+
+probe_current = 100e-6
+nplc = 2
+vlim = 1
+
+meas = {"I+": "G", "I-": "G"}
+
+sb.switch(meas)
+
+sm.enable_2_wire_probe(probe_current, nplc, vlim)
+time.sleep(2)
+
+c, v = sm.read_one()
+R = v / c
+sm.disable_probe_current()
+
+print("Current =",probe_current*10**(3),"mA")
+print("Recorded: Voltage =",v,"V","  Resistance =",R,"Ohms")
+
+sm.BEEP(300,0.5)
+plt.pause(0.5)
+sm.BEEP(400,0.5)
+plt.pause(0.5)
+sm.BEEP(500,0.5)
+
+
+sb.close()
+sm.close()
+
